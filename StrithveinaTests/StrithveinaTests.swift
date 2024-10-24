37c843ca41379d59d291792d8f3046eb0b9cee23
Callum Mackenzie <alexxander1611@gmail.com>
2024-10-24 01:30:59 -0700

    Initial Commit

---
+++ StrithveinaTests/StrithveinaTests.swift
@@ -0,0 +1,17 @@
+//
+//  StrithveinaTests.swift
+//  StrithveinaTests
+//
+//  Created by Callum Mackenzie on 2024-10-24.
+//
+
+import Testing
+@testable import Strithveina
+
+struct StrithveinaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
